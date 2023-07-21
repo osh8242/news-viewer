00efcf9136ec9afb6a4d7717224de293ae9e7b21
osh <osh8242@gmail.com>
2023-07-21 10:19:27 +0900

    230720 수업완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Categories from './components/Categories';
+import NewsList from './components/NewsList';
 
+/*
+https://jsonplaceholder.typicode.com/todos/1
+*/
 function App() {
+  // const [data, setData] = useState("안녕하세요");
+
+  // const onClick = async () => {
+  //   //서버에 자료를 얻어 설정하면 됨
+  //   // const response = await fetch(
+  //   //   "https://jsonplaceholder.typicode.com/todos/1"
+  //   // );
+  //   // const jsonData = await response.json();
+  //   // setData(JSON.stringify(jsonData));
+
+  //   try {
+  //     const response = await axios.get(
+  //       "https://newsapi.org/v2/top-headlines?country=kr&apiKey=ae3df86896064a709c97f5898afccbc5"
+  //     );
+  //     setData(JSON.stringify(response.data));
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+
+  //   // fetch("https://jsonplaceholder.typicode.com/todos/1")
+  //   //   .then(response => {
+  //   //     console.log(response);
+  //   //     if (response.status === 200) {
+  //   //       return response.json();
+  //   //     } else {
+  //   //       throw new Error("Something went wrong on api server!");
+  //   //     }
+  //   //   })
+  //   //   .then(response => {
+  //   //     setData(JSON.stringify(response));
+  //   //     console.log("응답자료 문자열 -> ", JSON.stringify(response));
+  //   //   })
+  //   //   .catch(error => {
+  //   //     console.error(error);
+  //   //   });
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Categories />
+      <NewsList />
+    </>
   );
 }
 
---
+++ src/components/NewsList.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { styled } from 'styled-components';
+import NewsItems from './NewsItems';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  //상태 변수로 관리 (목록, 로딩중)
+  //1. 서버에서 자료를 로딩한다 -> 비동기
+  //2. 서버에 요청한 자료가 로딩중 일때는 화면에 로딩중 ...
+  //3. 서버에 요청한 자료가 로딩 완료되면 출력
+  const [articles, setArticles] = useState(null); //[article, article, article, article];
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=ae3df86896064a709c97f5898afccbc5');
+    setArticles(response.data.articles);
+    //서버에 요청한 자료가 로딩 완료되면 완료 설정
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    try {
+      setLoading(true);
+      //   axios
+      //     .get(
+      //       "https://newsapi.org/v2/top-headlines?country=kr&apiKey=ae3df86896064a709c97f5898afccbc5"
+      //     )
+      //     .then(response => {
+      //       setArticles(response.data.articles);
+      //       //서버에 요청한 자료가 로딩 완료되면 완료 설정
+      //       setLoading(false);
+      //     });
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>서버에서 뉴스 로딩중 ...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return <NewsListBlock>서버에서 뉴스가 없습니다</NewsListBlock>;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItems article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
